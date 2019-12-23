64e531bc1b2760776d179e26484421a79bd9f6d0
Meteor <mg1827052@smail.nju.edu.cn>
2019-12-23 22:20:16 +0800

    拓扑排序和关键路径

--- Java/DataStructure/src/Graph/AdjListGraph.java
+++ Java/DataStructure/src/Graph/AdjListGraph.java
@@ -1,5 +1,7 @@
 package Graph;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.LinkedList;
 
 /**
@@ -51,7 +53,7 @@ public class AdjListGraph<T,E extends Number> extends Graph<E>{
 
     protected VertexNode<T,E>[] vertexes;//顶点数组
 
-    protected AdjListGraph(int vertexNum){//分配空间
+    public AdjListGraph(int vertexNum){//分配空间
         super(vertexNum);
         vertexes=new VertexNode[vertexNum];
     }
@@ -68,12 +70,12 @@ public class AdjListGraph<T,E extends Number> extends Graph<E>{
     public static AdjListGraph getTestInstance() {//构造测试数据实例
         AdjListGraph<String,Integer> graph= new AdjListGraph<String, Integer>(5);
         for (int i=0;i<5;i++){
-            VertexNode<String,Integer> node=new VertexNode("V"+(i+1));
+            VertexNode<String,Integer> node=new VertexNode("V"+i);
             graph.vertexes[i]=node;
         }
         try{
-            int[][] testData={{0,1,3},{0,3,4},{0,2,6},{1,0,3},
-                    {1,3,9},{2,0,6},{2,4,5},{3,0,4},{3,1,9},{4,2,5}};
+            int[][] testData={{0,1,1},{0,2,2},{1,2,3},{1,3,4},
+                    {2,3,5},{2,4,6},{3,4,7}};
             for (int i=0;i<testData.length;i++){
                 graph.addEdge(testData[i][0],testData[i][1],testData[i][2]);
             }
@@ -142,10 +144,133 @@ public class AdjListGraph<T,E extends Number> extends Graph<E>{
         return result;
     }
 
-    public void findMiniSpanningTree(boolean flag){
-        //求解图的最小生成树
-        if (flag){//用kruskal算法求解
-
+    /**
+     * 拓扑排序
+     */
+    public void topoSort(){
+        try {
+            LinkedList<Integer> result=topoSort(null);
+            if (result.size()==vertexNum)
+            {
+                System.out.print("该图的拓扑排序结果为：");
+                for (int i:result)
+                    System.out.print(vertexes[i].data+",");
+                System.out.println();
+            }
+            else
+                System.out.println("图中存在环，不存在拓扑排序");
+        }catch (Exception e){
+            System.out.println("目前拓扑排序只支持整形权重");
         }
+
+    }
+
+    /**
+     * 拓扑排序
+     * @param earlistStartTime 事件的最早开始时间
+     * @return 拓扑排序结果
+     */
+    protected LinkedList<Integer> topoSort(int[] earlistStartTime) throws Exception {
+        int[] in=new int[vertexNum];//统计每个节点的入度的数组
+        boolean[] visitFlag=new boolean[vertexNum];//标记节点是否已输出
+        //初始化
+        if (earlistStartTime!=null){
+            for (int i=0;i<vertexNum;i++)
+                earlistStartTime[i]=0;
+        }
+        for (int i=0;i<vertexNum;i++){
+            visitFlag[i]=false;
+            VertexNode node=vertexes[i];
+            ArcNode arcNode=node.firstArc;
+            while (arcNode!=null){
+                in[arcNode.nodeIndex]++;
+                arcNode=arcNode.next;
+            }
+        }
+        LinkedList<Integer> stack=new LinkedList<>();
+        LinkedList<Integer> result=new LinkedList<>();
+        //将入度为0的下标入栈
+        for (int i=0;i<vertexNum;i++)
+            if (in[i]==0)
+            {
+                stack.add(in[i]);
+                visitFlag[i]=true;
+            }
+        //开始拓扑排序
+        while (!stack.isEmpty()){
+            //弹栈
+            int curIndex=stack.pop();
+            result.push(curIndex);
+            visitFlag[curIndex]=true;
+            //将curIndex相连的节点的入度减1
+            ArcNode curArc=vertexes[curIndex].firstArc;
+            while (curArc!=null){
+                if (!visitFlag[curArc.nodeIndex])
+                {
+                    in[curArc.nodeIndex]--;
+                    //需要计算事件的最早开始时间
+                    if (earlistStartTime!=null){
+                        if (curArc.weight instanceof Integer){
+                            Integer weight=(Integer)curArc.weight;
+                            if (earlistStartTime[curIndex]+weight>earlistStartTime[curArc.nodeIndex])
+                                earlistStartTime[curArc.nodeIndex]=earlistStartTime[curIndex]+weight;
+                        }else
+                            throw new Exception("暂时只支撑整形权重");
+
+                    }
+                    //如果入度变为1,则入栈
+                    if (in[curArc.nodeIndex]==0){
+                        stack.add(curArc.nodeIndex);
+                        visitFlag[curArc.nodeIndex]=true;
+                    }
+                }
+                curArc=curArc.next;
+            }
+        }
+        return result;
+    }
+
+    public void keyPath(){
+        try{
+            int[] etv=new int[vertexNum];//事件的最早发生时间
+            LinkedList<Integer> topoResult=topoSort(etv);//拓扑排序结果
+            if (topoResult.size()==vertexNum){
+                int[] ltv=new int[vertexNum];//事件的最晚发生时间
+                //初始化ltv
+                for (int i=0;i<vertexNum;i++)
+                    ltv[i]=etv[vertexNum-1];
+                //求解ltv
+                while (!topoResult.isEmpty()){
+                    int curIndex=topoResult.pop();
+                    ArcNode curArc=vertexes[curIndex].firstArc;
+                    while (curArc!=null){
+                        Integer weight=(Integer)curArc.weight;
+                        if (ltv[curArc.nodeIndex]-weight<ltv[curIndex])
+                            ltv[curIndex]=ltv[curArc.nodeIndex]-weight;
+                        curArc=curArc.next;
+                    }
+                }
+                //求解每个活动的最早开始时间和最晚开始时间
+                System.out.print("AOE网中的关键活动为：");
+                for (int i=0;i<vertexNum;i++){
+                    ArcNode curArc=vertexes[i].firstArc;
+                    while (curArc!=null){
+                        int ete=etv[i];//活动的最早开始时间
+                        int lte=ltv[curArc.nodeIndex]-(Integer) curArc.weight;//活动的最晚开始时间
+                        if (ete==lte)
+                            System.out.print(vertexes[i].data.toString()+vertexes[curArc.nodeIndex].data+",");
+                        curArc=curArc.next;
+                    }
+                }
+                System.out.println();
+
+            }else {
+                System.out.println("图中存在环！");
+            }
+
+        }catch (Exception e){
+            System.out.println("目前关键路径只支持整形权重");
+        }
+
     }
 }
--- Java/DataStructure/src/Main.java
+++ Java/DataStructure/src/Main.java
@@ -3,18 +3,8 @@ import Graph.*;
 
 public class Main {
     public static void main(String[] args){
-        int[][] testData={{0,1,1},{0,2,5},{1,0,1},{1,2,3},{1,3,7},{1,4,5}
-                ,{2,0,5},{2,1,3},{2,4,1},{2,5,7},{3,1,7},{3,4,2},{3,6,3}
-                ,{4,1,5},{4,2,1},{4,3,2},{4,5,3},{4,6,6},{4,7,9},{5,2,7},{5,4,3},{5,7,5}
-                ,{6,3,3},{6,4,6},{6,7,2},{6,8,7},{7,4,9},{7,5,5},{7,6,2},{7,8,4}
-                ,{8,6,7},{8,7,4},
-        };
-        String[] vertexData={"V0","V1","V2","V3","V4","V5","V6","V7","V8"};
-        AdjMatrixGraph graph=new AdjMatrixGraph(testData,vertexData);
-        System.out.print(graph);
-        graph.traverse(false);
-        System.out.println();
-        graph.traverse(true);
-        graph.findMiniPath(0,6,false);
+        AdjListGraph graph=AdjListGraph.getTestInstance();
+        System.out.println(graph.toString());
+        graph.keyPath();
     }
 }
